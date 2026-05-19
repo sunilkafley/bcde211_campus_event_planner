a41b420386bbb602a7f421fe2d2a684e24508fab
Sunil Kafley <suk0424@arastudent.ac.nz>
2026-05-20 11:04:24 +1200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,29 @@
 const CACHE_NAME = "campus-event-planner-shell-v1";
 
+const APP_SHELL = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
 self.addEventListener("install", (event) => {
-    console.log("Service Worker installing.");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_SHELL);
+    })
+  );
 });
+
 self.addEventListener("activate", (event) => {
-    console.log("Service Worker activating.");
+  event.waitUntil(self.clients.claim());
 });
+
 self.addEventListener("fetch", (event) => {
-    console.log("Service Worker fetching:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
